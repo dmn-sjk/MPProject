3e52c89b44c4f8db08bddb042d96d6acdef0b6a4
dmn-sjk <sojka.damian9@gmail.com>
2023-03-23 19:34:57 +0100

    delete dockerfiles

--- MP_LOAM/mp_loam_build.sh
+++
@@ -1,5 +0,0 @@
-xhost +local:root
-
-# BUILD THE IMAGE
-ROS_IMAGE="mp/loam"
-docker build -f Dockerfile -t $ROS_IMAGE ./..
--- MP_ORB3/mp_orb3_build.sh
+++
@@ -1,5 +0,0 @@
-xhost +local:root
-
-# BUILD THE IMAGE
-ROS_IMAGE="mp/orb3"
-docker build -f Dockerfile -t $ROS_IMAGE ./..
